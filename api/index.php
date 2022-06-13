d96c88567ff404dd40b37e5242f2541aa946153a
ArtemKalugin <ArtemKalugin>
2022-06-13 21:29:40 +0700

    Настройка файла Index.php
    Написал функцию getMethod() для определения метода запроса и функцию
    getData() для получения данных из тела запроса, а также реализовал подключение
    роутеров

--- api/index.php
+++ api/index.php
@@ -1,2 +1,51 @@
 <?php
+    global $Link;
+        
+    header("Content-type: application/json");
+        
+    function getData($method) {
+        $data = new stdClass();
+        if ($method != "GET") {
+            $data->body = json_decode(file_get_contents('php://input'));
+        } 
+
+        $data->parameters = [];
+            $dataGet = $_GET;
+
+            foreach ($dataGet as $key => $value) {
+                if ($key != "q") {
+                    $data->parameters[$key] = $value;
+                }
+            }
+
+        return $data;
+    }
+
+    function getMethod() {
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    $Link = mysqli_connect("localhost", "backend", "221001", "backend");
+    if (!$Link) {
+        setHTTPStatus("500");
+    }
+
+    $url = isset($_GET["q"]) ? $_GET["q"] : "";
+    $url = rtrim($url, "/");
+    $urlList = explode("/", $url);
+
+    $router = $urlList[0];
+    $requestData = getData(getMethod());
+    $method = getMethod();
+
+    if(file_exists(realpath(dirname(__FILE__)) . "/routers/" . $router . ".php")) {
+        include_once "routers/" . $router . ".php";
+        route($method, $urlList, $requestData);
+    } else {
+        setHTTPStatus("404");
+    }
+
+    mysqli_close($Link);
+    return;
+
 ?>
