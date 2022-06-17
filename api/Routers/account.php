37234ae0380390c04421a5fd770d9760cc68d6fb
ArtemKalugin <ArtemKalugin>
2022-06-18 02:02:29 +0700

    Реализовал авторизацию пользователей
    Написал запрос авторизации, при успешной
    авторизации выдаётся токен

--- api/Routers/account.php
+++ api/Routers/account.php
@@ -48,6 +48,34 @@ function route($method, $urlData, $formData) {
             }
 
             break; 
+
+        case 'login':
+
+            $email = $requestData["Email"];
+            $password = hash("sha1", $formData["Password"]);
+
+            $user = $Link->query("SELECT id FROM users WHERE email='$email' AND password='$password'")->fetch_assoc();
+
+            if (!is_null($user)) {
+                $token = bin2hex(random_bytes(16));
+                $userID = $user["id"];
+                $validTime = time() + (10 * 60);
+                $validUntil = date('Y-m-d H:i:s', $validTime);
+
+                $userInsertResult = $Link->query("INSERT INTO tokens(value, userID, validUntil) VALUES('$token', '$userID', '$validUntil')");
+
+                if (!$userInsertResult) {
+                    setHTTPStatus("500");
+                } else {
+                    echo json_encode(["token" => $token]);
+                }
+            } else {
+                setHTTPStatus("409");
+            }
+           
+            break;
+        
+
         }
     }
 }
