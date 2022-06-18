f0d82840af2962fdba414f5cba54ac84129f1a5f
ArtemKalugin <ArtemKalugin>
2022-06-18 14:33:02 +0700

    Написал вспомогательную функцию requestUserID
    Функция проверяет существует ли пользователь с данным токеном

--- api/helpers/requester.php
+++ api/helpers/requester.php
@@ -1,4 +1,15 @@
 <?php
+function requestUserID($token) {
+    global $Link;
+
+    $userID = $Link->query("SELECT userID FROM tokens WHERE value='$token'")->fetch_assoc();
+    if (is_null($userID)) {
+        setHTTPStatus("500", "Не найдено");
+    } else {
+        return $userID;
+    }
+}
+
 function requestCompany($index) {
     global $Link;
     $company = $Link->query("SELECT * FROM companies WHERE id='$index'")->fetch_assoc();
